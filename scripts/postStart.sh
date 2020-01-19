1e37c9ac0260de8ffafa662db408ef82affc289b
James Vance <james.vance@jbhunt.com>
2020-01-18 21:12:15 -0600

    Fix

--- scripts/postStart.sh
+++ scripts/postStart.sh
@@ -6,6 +6,7 @@ REMOTE_JUKEBOX_ENABLED=$(tail /home/fpp/media/plugins/remote-falcon/remote_jukeb
 if [ "$REMOTE_FPP_ENABLED" = "true" ]; then
 	#/usr/bin/curl "http://fpp/runEventScript.php?scriptName=fpp_remote.sh" &
 	sh /home/fpp/media/plugins/remote-falcon/scripts/fpp_remote.sh &
+	/usr/bin/php /home/fpp/media/plugins/remote-falcon/fpp_remote.php &
 fi
 if [ "$REMOTE_JUKEBOX_ENABLED" = "true" ]; then
 	#/usr/bin/curl "http://fpp/runEventScript.php?scriptName=remote_jukebox.sh" &
