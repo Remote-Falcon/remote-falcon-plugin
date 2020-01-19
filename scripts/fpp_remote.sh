1827fe2b9613b9bd4313ea6502a57599c7f15889
James Vance <james.vance@jbhunt.com>
2020-01-18 19:22:52 -0600

    Sleepy time

--- scripts/fpp_remote.sh
+++ scripts/fpp_remote.sh
@@ -9,7 +9,9 @@ sleep 5
 SSHPASS='falcon' sshpass -e ssh -tt -o StrictHostKeyChecking=no fpp@$(cat /proc/sys/kernel/hostname) ssh -R 80:localhost:80 ssh.localhost.run > /home/fpp/media/plugins/remote-falcon/remote_url.txt 2>&1 & disown
 sleep 5
 REMOTEURLTXT=$(echo falcon | sudo tail /home/fpp/media/plugins/remote-falcon/remote_url.txt)
+sleep 3
 REMOTEURL=$(echo ${REMOTEURLTXT} | cut -d ' ' -f 13- | tr -d '\040\011\012\015')
+sleep 3
 echo falcon | sudo /usr/bin/curl -H "Content-Type: application/json" -X POST -d "{\"remoteKey\":\"${REMOTE_TOKEN}\",\"remoteURL\":\"${REMOTEURL}\"}" https://remotefalcon.com/cgi-bin/rmrghbsEvMhSH8LKuJydVn23pvsFKX/saveRemoteByKey.php
 
 echo "Complete!"
