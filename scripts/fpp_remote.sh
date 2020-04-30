0c576c853bee8af69acc095b4aa3f33c93ab2cb4
James Vance <james.vance@jbhunt.com>
2020-04-29 20:08:44 -0500

    Fix to ensure unique URLs

--- scripts/fpp_remote.sh
+++ scripts/fpp_remote.sh
@@ -3,10 +3,10 @@
 REMOTE_TOKEN=$(tail /home/fpp/media/plugins/remote-falcon/remote_token.txt)
 
 echo falcon | sudo apt-get install sshpass
-(sleep 5; echo yes; sleep 2; echo n;...) | SSHPASS='falcon' sshpass -e ssh -tt -o StrictHostKeyChecking=no fpp@$(cat /proc/sys/kernel/hostname) ssh -R 80:localhost:80 ssh.localhost.run & disown
+(sleep 5; echo yes; sleep 2; echo n;...) | SSHPASS='falcon' sshpass -e ssh -tt -o StrictHostKeyChecking=no fpp@$(cat /proc/sys/kernel/hostname) ssh -R 80:localhost:80 ${REMOTE_TOKEN}@ssh.localhost.run & disown
 echo falcon | sudo rm -f /home/fpp/media/plugins/remote-falcon/remote_url.txt
 sleep 5
-SSHPASS='falcon' sshpass -e ssh -tt -o StrictHostKeyChecking=no fpp@$(cat /proc/sys/kernel/hostname) ssh -R 80:localhost:80 ssh.localhost.run > /home/fpp/media/plugins/remote-falcon/remote_url.txt 2>&1 & disown
+SSHPASS='falcon' sshpass -e ssh -tt -o StrictHostKeyChecking=no fpp@$(cat /proc/sys/kernel/hostname) ssh -R 80:localhost:80 ${REMOTE_TOKEN}@ssh.localhost.run > /home/fpp/media/plugins/remote-falcon/remote_url.txt 2>&1 & disown
 sleep 5
 REMOTEURLTXT=$(echo falcon | sudo tail /home/fpp/media/plugins/remote-falcon/remote_url.txt)
 sleep 3
