380a7be16b08a1cdf4fa93470b3859c6eda74621
James Vance <james.vance@jbhunt.com>
2020-05-02 12:57:49 -0500

    Possible Jukebox fix

--- scripts/remote_jukebox.sh
+++ scripts/remote_jukebox.sh
@@ -1,44 +1,45 @@
 #!/bin/bash
 
 REMOTE_TOKEN=$(tail /home/fpp/media/plugins/remote-falcon/remote_token.txt)
+NORMAL_SCHEDULE = "true"
 
 while [ true ]
 do
 PLAYLISTNAME=$(/usr/bin/curl -H "Content-Type: application/json" -X POST -d "{\"remoteToken\":\"${REMOTE_TOKEN}\"}" https://remotefalcon.com/cgi-bin/rmrghbsEvMhSH8LKuJydVn23pvsFKX/fetchCurrentPlaylistFromQueue.php)
-PLAYLISTUPONSILENCE=$(/usr/bin/curl -H "Content-Type: application/json" -X POST -d "{\"remoteToken\":\"${REMOTE_TOKEN}\"}" https://remotefalcon.com/cgi-bin/rmrghbsEvMhSH8LKuJydVn23pvsFKX/fetchPlaylistUponSilence.php)
-if [ "${PLAYLISTNAME}" != "null" ]; then
-	STATUS=$(fpp -s | cut -d',' -f2)
-	if [ -z "${STATUS}" ]; then
-		echo "Error with status value" >&2
-		exit 1
-	fi
-	case ${STATUS} in
-		0)
+
+STATUS=$(fpp -s | cut -d',' -f2)
+if [ -z "${STATUS}" ]; then
+	echo "Error with status value" >&2
+	exit 1
+fi
+
+case ${STATUS} in
+	#Idle
+	0)
+		"${NORMAL_SCHEDULE}" = "true"
+		if [ "${PLAYLISTNAME}" != "null" ]; then
+			"${NORMAL_SCHEDULE}" = "false"
 			echo "Starting ${PLAYLISTNAME}"
 			fpp -P "${PLAYLISTNAME}" ${STARTITEM}
 			/usr/bin/curl -H "Content-Type: application/json" -X POST -d "{\"remoteToken\":\"${REMOTE_TOKEN}\"}" https://remotefalcon.com/cgi-bin/rmrghbsEvMhSH8LKuJydVn23pvsFKX/updatePlaylistQueue.php
-			;;
-		1)
-			;;
-		2|*)
-			;;
+		fi
+		;;
+	#Playing
+	1)
+		if [ "${NORMAL_SCHEDULE}" = "true" && "${PLAYLISTNAME}" != "null" ]; then
+			"${NORMAL_SCHEDULE}" = "false"
+			echo "Starting ${PLAYLISTNAME}"
+			fpp -P "${PLAYLISTNAME}" ${STARTITEM}
+			/usr/bin/curl -H "Content-Type: application/json" -X POST -d "{\"remoteToken\":\"${REMOTE_TOKEN}\"}" https://remotefalcon.com/cgi-bin/rmrghbsEvMhSH8LKuJydVn23pvsFKX/updatePlaylistQueue.php
+		elif [ "${NORMAL_SCHEDULE}" = "false" && "${PLAYLISTNAME}" == "null" ]; then
+			echo "Resuming Schedule"
+			"${NORMAL_SCHEDULE}" = "true"
+			fpp -c graceful
+		fi
+		;;
+	#Stopping Gracefully
+	2|*)
+		;;
 	esac
-# elif [ "${PLAYLISTUPONSILENCE}" != "" ]; then
-# 	STATUS=$(fpp -s | cut -d',' -f2)
-# 	if [ -z "${STATUS}" ]; then
-# 		echo "Error with status value" >&2
-# 		exit 1
-# 	fi
-# 	case ${STATUS} in
-# 		0)
-# 			echo "Starting ${PLAYLISTUPONSILENCE}"
-# 			fpp -P "${PLAYLISTUPONSILENCE}" ${STARTITEM}
-# 			;;
-# 		1)
-# 			;;
-# 		2|*)
-# 			;;
-# 	esac
-fi
 sleep 5
 done
