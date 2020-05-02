cfbd1c0e22a744bdfd2134390442676ab5f067ab
James Vance <james.vance@jbhunt.com>
2020-05-02 14:09:41 -0500

    Reload schedule if no playlist in Jukebox

--- scripts/remote_jukebox.sh
+++ scripts/remote_jukebox.sh
@@ -1,45 +1,29 @@
 #!/bin/bash
 
 REMOTE_TOKEN=$(tail /home/fpp/media/plugins/remote-falcon/remote_token.txt)
-NORMAL_SCHEDULE=true
 
 while [ true ]
 do
 PLAYLISTNAME=$(/usr/bin/curl -H "Content-Type: application/json" -X POST -d "{\"remoteToken\":\"${REMOTE_TOKEN}\"}" https://remotefalcon.com/cgi-bin/rmrghbsEvMhSH8LKuJydVn23pvsFKX/fetchCurrentPlaylistFromQueue.php)
-
-STATUS=$(fpp -s | cut -d',' -f2)
-if [ -z "${STATUS}" ]; then
-	echo "Error with status value" >&2
-	exit 1
-fi
-
-case ${STATUS} in
-	#Idle
-	0)
-		NORMAL_SCHEDULE=true
-		if [ "${PLAYLISTNAME}" != "null" ]; then
-			NORMAL_SCHEDULE = "false"
+if [ "${PLAYLISTNAME}" != "null" ]; then
+	STATUS=$(fpp -s | cut -d',' -f2)
+	if [ -z "${STATUS}" ]; then
+		echo "Error with status value" >&2
+		exit 1
+	fi
+	case ${STATUS} in
+		0)
 			echo "Starting ${PLAYLISTNAME}"
 			fpp -P "${PLAYLISTNAME}" ${STARTITEM}
 			/usr/bin/curl -H "Content-Type: application/json" -X POST -d "{\"remoteToken\":\"${REMOTE_TOKEN}\"}" https://remotefalcon.com/cgi-bin/rmrghbsEvMhSH8LKuJydVn23pvsFKX/updatePlaylistQueue.php
-		fi
-		;;
-	#Playing
-	1)
-		if [ "$NORMAL_SCHEDULE" = true && "${PLAYLISTNAME}" != "null" ]; then
-			NORMAL_SCHEDULE=false
-			echo "Starting ${PLAYLISTNAME}"
-			fpp -P "${PLAYLISTNAME}" ${STARTITEM}
-			/usr/bin/curl -H "Content-Type: application/json" -X POST -d "{\"remoteToken\":\"${REMOTE_TOKEN}\"}" https://remotefalcon.com/cgi-bin/rmrghbsEvMhSH8LKuJydVn23pvsFKX/updatePlaylistQueue.php
-		elif [ "$NORMAL_SCHEDULE" = false && "${PLAYLISTNAME}" == "null" ]; then
-			echo "Resuming Schedule"
-			"$NORMAL_SCHEDULE"=true
-			fpp -c graceful
-		fi
-		;;
-	#Stopping Gracefully
-	2|*)
-		;;
+			;;
+		1)
+			;;
+		2|*)
+			;;
 	esac
-sleep 5
+else
+	fpp -R
+fi
+sleep 3
 done
