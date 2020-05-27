d7d5eb639862252459f596682cb78bd5d15e600d
James Vance <james.vance@jbhunt.com>
2020-05-26 21:43:30 -0500

    API Update

--- scripts/remote_fpp.sh
+++ scripts/remote_fpp.sh
@@ -5,14 +5,15 @@ echo "Starting Remote FPP"
 remoteToken=$(tail /home/fpp/media/plugins/remote-falcon/remote_token.txt)
 
 currentlyPlayingInRF=""
-viewerControlMode=$(/usr/bin/curl https://remotefalcon.com/remotefalcon/api/viewerControlMode/${remoteToken} | python -c "import sys, json; print json.load(sys.stdin)['viewerControlMode']")
+viewerControlMode=$(/usr/bin/curl -H "remotetoken: ${remoteToken}" https://remotefalcon.com/remotefalcon/api/viewerControlMode | python -c "import sys, json; print json.load(sys.stdin)['viewerControlMode']")
 echo "viewerControlMode: ${viewerControlMode}" 
 while [ true ]
 do
 	currentlyPlaying=$(fpp -s | cut -d',' -f4)
 	if [ "$currentlyPlaying" != "$currentlyPlayingInRF" ]; then
 		echo "Updating current playing playlist to ${currentlyPlaying}"
-		currentlyPlayingInRF=$(/usr/bin/curl -H "Content-Type: application/json" -X POST -d "{\"remoteToken\":\"${remoteToken}\",\"playlist\":\"${currentlyPlaying}\"}" https://remotefalcon.com/remotefalcon/api/updateWhatsPlaying)
+		/usr/bin/curl -H "Content-Type: application/json" -H "remotetoken: ${remoteToken}" -X POST -d "{\"playlist\":\"${currentlyPlaying}\"}" https://remotefalcon.com/remotefalcon/api/updateWhatsPlaying
+		currentlyPlayingInRF=$currentlyPlaying;
 	fi
 	fppSchedulePlaying=$(fpp -s | cut -d',' -f14)
 	case ${fppSchedulePlaying} in
@@ -24,19 +25,19 @@ do
 		#Schedule playing, fetch next playlist
 		1)
 			if [ "${viewerControlMode}" = "voting" ]; then
-				playlist=$(/usr/bin/curl https://remotefalcon.com/remotefalcon/api/highestVotedPlaylist/${remoteToken} | python -c "import sys, json; print json.load(sys.stdin)['winningPlaylist']")
-				if [ "${playlist}" != "" ]; then
+				playlist=$(/usr/bin/curl -H "remotetoken: ${remoteToken}" https://remotefalcon.com/remotefalcon/api/highestVotedPlaylist | python -c "import sys, json; print json.load(sys.stdin)['winningPlaylist']")
+				if [ "${playlist}" != "None" ]; then
 					echo "Starting Request for ${playlist}"
 					fpp -P "${playlist}"
 				else
 					echo "No playlist found"
 				fi
 			else
-				playlist=$(/usr/bin/curl https://remotefalcon.com/remotefalcon/api/nextPlaylistInQueue/${remoteToken} | python -c "import sys, json; print json.load(sys.stdin)['nextPlaylist']")
-				if [ "${playlist}" != "" ]; then
+				playlist=$(/usr/bin/curl -H "remotetoken: ${remoteToken}" https://remotefalcon.com/remotefalcon/api/nextPlaylistInQueue | python -c "import sys, json; print json.load(sys.stdin)['nextPlaylist']")
+				if [ "${playlist}" != "None" ]; then
 					echo "Starting Request for ${playlist}"
 					fpp -P "${playlist}"
-					/usr/bin/curl -H "Content-Type: application/json" -X POST -d "{\"remoteToken\":\"${remoteToken}\"}" https://remotefalcon.com/remotefalcon/api/updatePlaylistQueue
+					/usr/bin/curl -H "Content-Type: application/json" -H "remotetoken: ${remoteToken}" -X POST https://remotefalcon.com/remotefalcon/api/updatePlaylistQueue
 				else
 					echo "No playlist found"
 				fi
