56d61a88d072d7d318f8859270eb6c975f44ac60
James Vance <james.vance@jbhunt.com>
2020-06-01 15:44:41 -0500

    Plugin version

--- scripts/postStart.sh
+++ scripts/postStart.sh
@@ -2,6 +2,7 @@
 REMOTE_FPP_ENABLED=$(tail /home/fpp/media/plugins/remote-falcon/remote_fpp_enabled.txt)
 
 /usr/bin/php /home/fpp/media/plugins/remote-falcon/remote_playlist_sync.php &
+/usr/bin/php /home/fpp/media/plugins/remote-falcon/plugin_version.php &
 
 if [ "$REMOTE_FPP_ENABLED" = "true" ]; then
 	sh /home/fpp/media/plugins/remote-falcon/scripts/remote_fpp.sh &
