463e46b184289fa47c0422af68009ccd22fd5d13
James Vance <james.vance@jbhunt.com>
2020-10-08 15:17:31 -0500

    More donwload

--- download_log.php
+++ download_log.php
@@ -1,15 +1,31 @@
 <?php
-$name= $_GET['name'];
-header('Content-Description: File Transfer');
-header('Content-Type: application/force-download');
-header("Content-Disposition: attachment; filename=\"" . basename($name) . "\";");
-header('Content-Transfer-Encoding: binary');
-header('Expires: 0');
-header('Cache-Control: must-revalidate');
-header('Pragma: public');
-header('Content-Length: ' . filesize($name));
-ob_clean();
-flush();
-readfile("/home/fpp/media/plugins/remote-falcon/logs/".$name);
-exit;
+if(isset($_REQUEST["file"])){
+    // Get parameters
+    $file = urldecode($_REQUEST["file"]); // Decode URL-encoded string
+
+    /* Test whether the file name contains illegal characters
+    such as "../" using the regular expression */
+    if(preg_match('/^[^.][-a-z0-9_.]+[a-z]$/i', $file)){
+        $filepath = "/home/fpp/media/plugins/remote-falcon/logs/" . $file;
+
+        // Process download
+        if(file_exists($filepath)) {
+            header('Content-Description: File Transfer');
+            header('Content-Type: application/octet-stream');
+            header('Content-Disposition: attachment; filename="'.basename($filepath).'"');
+            header('Expires: 0');
+            header('Cache-Control: must-revalidate');
+            header('Pragma: public');
+            header('Content-Length: ' . filesize($filepath));
+            flush(); // Flush system output buffer
+            readfile($filepath);
+            die();
+        } else {
+            http_response_code(404);
+	        die();
+        }
+    } else {
+        die("Invalid file name!");
+    }
+}
 ?>
