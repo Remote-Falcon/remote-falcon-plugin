93ba963ae39958a6619e881bf6441bb0c05e9509
James Vance <james.vance@jbhunt.com>
2020-10-08 16:21:10 -0500

    Echos

--- remote_falcon.php
+++ remote_falcon.php
@@ -327,13 +327,18 @@ echo "
 if (isset($_POST['downloadLog1'])) {
 	$date = date("Y-m-d");
 	$file = '/home/fpp/media/plugins/remote-falcon/logs/' . $date . '.txt';
-	header('Content-Type: application/octet-stream');
-	header('Content-Disposition: attachment; filename=' . $file);
-	header('Expires: 0');
-	header('Cache-Control: must-revalidate');
-	header('Pragma: public');
-	header('Content-Length: ' . filesize($file));
-	readfile($file);
+	echo $file;
+	if(!file_exists($file)){
+    echo 'file not found';
+} else {
+    header('Content-Type: application/octet-stream');
+    header('Content-Disposition: attachment; filename=' . $file);
+    header('Expires: 0');
+    header('Cache-Control: must-revalidate');
+    header('Pragma: public');
+    header('Content-Length: ' . filesize($file));
+    readfile($file);
+}
 }
 
 if (isset($_POST['downloadLog2'])) {
