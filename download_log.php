b820bf411aa84b0106070b5974c86eb04d11a19e
James Vance <james.vance@jbhunt.com>
2020-10-08 15:54:33 -0500

    Hope this one works...

--- download_log.php
+++ download_log.php
@@ -1,17 +1,20 @@
 <?php
-$file = basename($_GET['file']);
-$file = '/home/fpp/media/plugins/remote-falcon/logs/'.$file;
+if (isset($argc)) {
+    $fullFile = '/home/fpp/media/plugins/remote-falcon/logs/' . $argv[1] . '.txt';
+    echo $fullFile;
 
-if(!file_exists($file)){ // file does not exist
-    die('file not found');
-} else {
-    header("Cache-Control: public");
-    header("Content-Description: File Transfer");
-    header("Content-Disposition: attachment; filename=$file");
-    header("Content-Type: application/zip");
-    header("Content-Transfer-Encoding: binary");
-
-    // read the file from disk
-    readfile($file);
+    if(!file_exists($fullFile)){
+        die('file not found');
+    } else {
+        header('Content-Type: application/octet-stream');
+        header('Content-Disposition: attachment; filename=' . $fullFile);
+        header('Expires: 0');
+        header('Cache-Control: must-revalidate');
+        header('Pragma: public');
+        header('Content-Length: ' . filesize($fullFile));
+        readfile($fullFile);
+    }
+}else {
+    echo "Args not set";
 }
 ?>
