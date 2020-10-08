380c33dec74955b0eaee09e1c2ea697ce2dea49b
James Vance <james.vance@jbhunt.com>
2020-10-08 15:36:18 -0500

    New download

--- download_log.php
+++ download_log.php
@@ -1,31 +1,17 @@
 <?php
-if(isset($_REQUEST["file"])){
-    // Get parameters
-    $file = urldecode($_REQUEST["file"]); // Decode URL-encoded string
+$file = basename($_GET['file']);
+$file = '/home/fpp/media/plugins/remote-falcon/logs/'.$file;
 
-    /* Test whether the file name contains illegal characters
-    such as "../" using the regular expression */
-    if(preg_match('/^[^.][-a-z0-9_.]+[a-z]$/i', $file)){
-        $filepath = "home/fpp/media/plugins/remote-falcon/logs/" . $file;
+if(!file_exists($file)){ // file does not exist
+    die('file not found');
+} else {
+    header("Cache-Control: public");
+    header("Content-Description: File Transfer");
+    header("Content-Disposition: attachment; filename=$file");
+    header("Content-Type: application/zip");
+    header("Content-Transfer-Encoding: binary");
 
-        // Process download
-        if(file_exists($filepath)) {
-            header('Content-Description: File Transfer');
-            header('Content-Type: application/octet-stream');
-            header('Content-Disposition: attachment; filename="'.basename($filepath).'"');
-            header('Expires: 0');
-            header('Cache-Control: must-revalidate');
-            header('Pragma: public');
-            header('Content-Length: ' . filesize($filepath));
-            flush(); // Flush system output buffer
-            readfile($filepath);
-            die();
-        } else {
-            http_response_code(404);
-	        die();
-        }
-    } else {
-        die("Invalid file name!");
-    }
+    // read the file from disk
+    readfile($file);
 }
 ?>
