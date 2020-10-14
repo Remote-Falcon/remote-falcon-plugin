7f84ca6bbd1ea831531add2a1e24e07f4ae381fe
Poporacer <46263877+Poporacer@users.noreply.github.com>
2020-10-14 11:44:02 -0600

    Update postStart.sh
    Change to work with settings files

--- scripts/postStart.sh
+++ scripts/postStart.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-REMOTE_FPP_ENABLED=$(tail /home/fpp/media/plugins/remote-falcon/remote_fpp_enabled.txt)
-
-/usr/bin/php /home/fpp/media/plugins/remote-falcon/plugin_version.php &
+ . /opt/fpp/scripts/common
+CFGFILE= /home/fpp/media/config/plugin.remote-falcon
+REMOTE_FPP_ENABLED=$(getSetting "remote_fpp_enabled")
 
 if [ "$REMOTE_FPP_ENABLED" = "true" ]; then
 	/usr/bin/php /home/fpp/media/plugins/remote-falcon/remote_fpp.php &
