592cb2a01c6528e2b04b2a15f7ac2d4042a497ac
James Vance <whitesoup12@gmail.com>
2021-09-02 20:18:06 -0500

    Remove chmod and change branch

--- scripts/postStart.sh
+++ scripts/postStart.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-chmod 777 -R /home/fpp/media/plugins/remote-falcon/commands
-
 /usr/bin/php /home/fpp/media/plugins/remote-falcon/plugin_version.php &
 /usr/bin/php /home/fpp/media/plugins/remote-falcon/remote_fpp.php &
 
