e72fbccd5b20d992921f476d1bba81d573dfb0bb
James Vance <whitesoup12@gmail.com>
2025-06-12 21:53:31 -0500

    Add domain for CSP

--- scripts/fpp_install.sh
+++ scripts/fpp_install.sh
@@ -2,6 +2,10 @@
 
 # Mark to reboot
 . ${FPPDIR}/scripts/common
+
+# Add required Apache CSP (Content-Security-Policy allowed domains
+${FPPDIR}/scripts/ManageApacheContentPolicy.sh add default-src https://remotefalcon.com
+
 setSetting restartFlag 1
 
 #fpp_install
